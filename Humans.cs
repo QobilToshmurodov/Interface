3142a834bad328d271c2fe6d7218dc3a983d3747
QobilToshmurodov <qobiltoshmurodov289@gmail.com>
2024-12-13 15:52:58 +0500

    first commit

---
+++ Humans.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class Humans: ISleepable
+    {
+        public string Name { get; set; }
+        public byte Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public void See()
+        {
+            Console.WriteLine("Ko'ryapman...");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Uxlayapman...");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Humans humans = new Humans();
+            humans.Sleep();
+        }
+    }
+}
